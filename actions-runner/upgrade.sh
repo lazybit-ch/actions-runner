7f91f44a36cdb8d0ba614db1b6621c7a2a562396
Bradley Massey <massey.bradley@gmail.com>
2020-09-28 14:57:55 +0200

    dev: disabled local dep update

--- actions-runner/upgrade.sh
+++ actions-runner/upgrade.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-helm repo add lazybit https://chartmuseum.lazybit.ch \
-    --username=${CHARTMUSEUM_USERNAME} \
-    --password=${CHARTMUSEUM_PASSWORD}
-helm repo update
-helm dependency update
+# helm repo add lazybit https://chartmuseum.lazybit.ch \
+#     --username=${CHARTMUSEUM_USERNAME} \
+#     --password=${CHARTMUSEUM_PASSWORD}
+# helm repo update
+# helm dependency update
 helm upgrade --install actions-runner \
     --set global.storageClass=nfs-client \
     --set github.username=${GITHUB_USERNAME} \
@@ -33,5 +33,6 @@ helm upgrade --install actions-runner \
     --set dind.resources.limits.cpu="2" \
     --set dind.livenessProbe.enabled=false \
     --set dind.readinessProbe.enabled=false \
+    --dry-run \
     . \
     --wait
